98f2b3699aedce52756ed75c93271d48f60d53b4
azlymoon <ispetrovanov@edu.hse.ru>
2022-02-07 18:01:04 +0300

    Добавил дороги между комнатами

--- map.py
+++ map.py
@@ -1,5 +1,60 @@
 import random
 import copy
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (item, priority))
+
+    def get(self):
+        return heapq.heappop(self.elements)[0]
+
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def neighbors(dist, point):
+    ddx = [0, 1, 0, -1]
+    ddy = [-1, 0, 1, 0]
+    y, x = point
+    neighbors = []
+    for i in range(len(ddy)):
+        if 0 < y + ddy[i] < len(dist) and 0 < x + ddx[i] < len(dist[0]):
+            neighbors.append((y + ddy[i], x + ddx[i]))
+    return neighbors
+
+
+def a_star_search(dist, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    while frontier.empty() is False:
+        current = frontier.get()
+        if current == goal:
+            break
+        for next in neighbors(dist, current):
+            y, x = next
+            new_cost = cost_so_far[current] + dist[y][x]
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from
 
 
 class Map:
@@ -16,7 +71,7 @@ class Map:
         print('-' * 150)
         for y in range(self.height):
             for x in range(self.width):
-                print('@' if self.map[y][x] is True else '#', end='')
+                print('@' if self.map[y][x] is True else '#', end=' ')
             print()
         print('-' * 150)
 
@@ -25,7 +80,8 @@ class Map:
             print('-' * 150)
             for y in range(len(dist)):
                 for x in range(len(dist[0])):
-                    print(' ' if dist[y][x] == self.cost_room else '$' if dist[y][x] == self.cost_frontier or dist[y][x] == self.cost_room_wall else '#', end=' ')
+                    print(' ' if dist[y][x] == self.cost_room else '$' if dist[y][x] == self.cost_frontier or dist[y][
+                        x] == self.cost_room_wall else '#', end=' ')
                 print()
             print('-' * 150)
 
@@ -33,7 +89,8 @@ class Map:
             width_d = self.width + 2
             height_d = self.height + 2
             dist = [[self.cost_frontier] * width_d if i == 0 or i == height_d - 1
-                    else [self.cost_frontier] + [self.cost_wall] * self.width + [self.cost_frontier] for i in range(height_d)]
+                    else [self.cost_frontier] + [self.cost_wall] * self.width + [self.cost_frontier] for i in
+                    range(height_d)]
             return dist
 
         def get_all_coord():
@@ -41,6 +98,14 @@ class Map:
             for y in range(len(dist)):
                 for x in range(len(dist[0])):
                     coords.append((y, x))
+            return coords
+
+        def get_field_coord():
+            coords = []
+            for y in range(len(dist)):
+                for x in range(len(dist[0])):
+                    if dist[y][x] == self.cost_room:
+                        coords.append((y, x))
             return coords
 
         def make_room(max_width_rect=8, max_height_rect=8):
@@ -94,13 +159,13 @@ class Map:
                 rect = make_rectangle()
                 gluing(room, field_room, rect)
 
-            return room, field_room
+            return field_room
 
-        def lock_neighbors(x, y):
+        def lock_neighbors(y, x):
             for i in range(len(ddy)):
-                for j in range(len(ddx)):
-                    if dist[y + ddy[i]][x + ddx[j]] == self.cost_wall:
-                        dist[y + ddy[i]][x + ddx[j]] = self.cost_room_wall
+                if 0 < y + ddy[i] < len(dist) and 0 < x + ddx[i] < len(dist[0]) \
+                        and dist[y + ddy[i]][x + ddx[i]] == self.cost_wall:
+                    dist[y + ddy[i]][x + ddx[i]] = self.cost_room_wall
 
         def paste_room_to_dist(dist, field_dist):
             new_dist = copy.deepcopy(dist)
@@ -132,7 +197,30 @@ class Map:
                     dist = copy.deepcopy(new_dist)
                     field_dist = copy.deepcopy(new_field_dist)
 
+                    # coords_field_dist = get_field_coord()
+                    # if len(coords_field_dist):
+                    #     create_tunnel(i, j, coords_field_dist)
             return dist, field_dist, lc_field_dist
+
+        def recovery_road(came_from, current, road):
+            if came_from[current] is None:
+                return road
+            road.append(came_from[current])
+            return recovery_road(came_from, road[-1], road)
+
+        def create_tunnel(coords_field_dist, lc_field_dist):
+            # coords_field_dist = get_field_coord()
+            start = lc_field_dist[random.randint(0, len(lc_field_dist) - 1)]
+            goal = coords_field_dist[random.randint(0, len(coords_field_dist) - 1)]
+            came_from = a_star_search(dist, start, goal)
+            road = recovery_road(came_from, goal, [])
+            for coord in road:
+                y, x = coord
+                dist[y][x] = self.cost_room
+                lock_neighbors(y, x)
+                # draw_in_terminal_dist()
+            # draw_in_terminal_dist()
+            # print(road)
 
         dist = init_dist()
         field_dist = []
@@ -141,13 +229,21 @@ class Map:
         ddy = [-1, 0, 1, 0]
 
         count_room = 10
-        for _ in range(count_room):
-            room, field_room = make_room()
+        for i in range(count_room):
+            field_room = make_room()
+            coords_field_dist = get_field_coord()
             dist, field_dist, lc_field_dist = paste_room_to_dist(dist, field_dist)
             for coord in lc_field_dist:
-                lock_neighbors(coord[1], coord[0])
-        draw_in_terminal_dist()
-        # draw_in_terminal_dist(room)
+                lock_neighbors(coord[0], coord[1])
+            if i != 0 and len(lc_field_dist):
+                create_tunnel(coords_field_dist, lc_field_dist)
+        # draw_in_terminal_dist()
+        map = [[0] * len(dist[0]) for _ in range(len(dist))]
+        for y in range(len(dist)):
+            for x in range(len(dist[0])):
+                map[y][x] = (True if dist[y][x] == self.cost_room else False)
+
+        return map
 
 
 map = Map()
