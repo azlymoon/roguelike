f6a7eadd5ee6f3aae90bb430a0dbc8f3201aa93f
azlymoon <ispetrovanov@edu.hse.ru>
2022-05-09 12:12:16 +0300

    прикрутил меню

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from Player import Player
 from map import Map
+from menu import show_menu, print_text, pause
 from CameraGroup import CameraGroup
 from mobs import Flying_eye, Goblin, Mushroom
 from projectile import Flying_eye_projectile, Goblin_projectile, Mushroom_projectile
+from math import sqrt
 
 pygame.init()
 
@@ -20,87 +22,13 @@ BLUE = (0, 0, 255)
 GREY = (47, 79, 79)
 
 
-class Button:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.inactive_clr = (13, 162, 58)
-        self.active_clr = (23, 204, 58)
-
-    def draw(self, x, y, message, action=None, font_size=30):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-
-        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
-            pygame.draw.rect(GameManager.screen, self.active_clr, (x, y, self.width, self.height))
-
-            if click[0] == 1:
-              # pygame.mixer.Sound.play(button_sound)
-                pygame.time.delay(300)
-                if action is not None:
-                    if action == quit:
-                        pygame.quit()
-                        quit()
-                    else:
-                        action()
-        else:
-            pygame.draw.rect(GameManager.screen, self.inactive_clr, (x, y, self.width, self.height))
-
-        print_text(message=message, x=x+10, y=y+10, font_size=font_size)
-
-
-def show_menu():
-    menu_bckgr = pygame.image.load('menu.jpg')
-    menu_bckgr = pygame.transform.scale(menu_bckgr, (1280, 720))
-
-    start_btn = Button(265, 80)
-    quit_btn = Button(245, 80)
-
-    show = True
-
-    while show:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        GameManager.screen.blit(menu_bckgr, (0, 0))
-        start_btn.draw(270, 200, 'start game', GameManager.run, 50)
-        quit_btn.draw(280, 300, 'quit game', quit, 50)
-
-        pygame.GameManager.screen.update()
-        GameManager.clock.tick(30)
-
-
-def print_text(message, x, y, font_color=(0, 0, 0), font_type='Empirecrown.ttf', font_size=30):
-    font_type = pygame.font.Font(font_type, font_size)
-    text = font_type.render(message, True, font_color)
-    GameManager.screen.blit(text, (x, y))
-
-
-def pause():
-    paused = True
-    while paused:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        print_text('paused. press ENTER to continue', 160, 300)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:
-            paused = False
-
-        pygame.screen.update()
-
-
 class GameManager:
     def __init__(self):
         pygame.display.set_caption("My Game")
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
         self.entities = pygame.sprite.Group()
-        self.state = "in_menu"
+        self.state = "menu"
         self.map = None
         self.visible_sprites = None
         self.player = None
@@ -132,56 +60,64 @@ class GameManager:
     def set_state(self, state):
         self.state = state
 
+    def start_game(self):
+        self.state = 'game_running'
+        self.run()
+
     def run(self):
-        show_menu()
-        self.init_map()
+        if self.state == 'menu':
+            show_menu(self)
+        elif self.state == 'game_running':
+            self.init_map()
+            # Отрисовка карты в консоль
+            print('-' * 150)
+            for y in range(len(self.map)):
+                for x in range(len(self.map[0])):
+                    print(' ' if self.map[y][x] is True else '#', end=' ')
+                print()
+            print('-' * 150)
 
-        # Отрисовка карты в консоль
-        print('-' * 150)
-        for y in range(len(self.map)):
-            for x in range(len(self.map[0])):
-                print(' ' if self.map[y][x] is True else '#', end=' ')
-            print()
-        print('-' * 150)
+            running = True
+            while running:
+                # Держим цикл на правильной скорости
+                self.clock.tick(FPS)
+                # Ввод процесса (события)
+                for event in pygame.event.get():
+                    # check for closing window
+                    if event.type == pygame.QUIT:
+                        running = False
+                for mob in self.mobs:
+                    if mob.projectile.status == "explode":
+                        mob.projectile.kill()
+                    if (sqrt((self.player.coordx - mob.coordx) ** 2 + (self.player.coordy - mob.coordy) ** 2)) <= 160:
+                        if mob.projectile.status == 'explode':
+                            mob.create_projectile((mob.coordx, mob.coordy), (self.player.coordx, self.player.coordy))
+                        else:
+                            self.visible_sprites.add(mob.projectile)
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_ESCAPE]:
+                    pause(self)
 
-        running = True
-        while running:
-            # Держим цикл на правильной скорости
-            self.clock.tick(FPS)
-            # Ввод процесса (события)
-            for event in pygame.event.get():
-                # check for closing window
-                if event.type == pygame.QUIT:
-                    running = False
-            for mob in self.mobs:
-                if mob.projectile.status == "explode":
-                    mob.projectile.kill()
-                if (
-                        sqrt((self.player.coordx - mob.coordx) ** 2 + (self.player.coordy - mob.coordy) ** 2)) <= 160:
-                    if mob.projectile.status == 'explode':
-                        mob.create_projectile((mob.coordx, mob.coordy), (self.player.coordx, self.player.coordy))
-                    else:
-                        self.visible_sprites.add(mob.projectile)
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_ESCAPE]:
-                pause()
+                # Обновление
+                # self.entities.update()
+                self.visible_sprites.update()
+                # Рендеринг
 
-            # Обновление
-            # self.entities.update()
-            self.visible_sprites.update()
-            # Рендеринг
+                self.screen.fill(BLACK)
+                # self.screen.fill(BLACK)
+                # self.screen.blit()
+                # self.map_surface.draw(self.screen)
 
-            self.screen.fill(BLACK)
-            # self.screen.fill(BLACK)
-            # self.screen.blit()
-            # self.map_surface.draw(self.screen)
+                self.visible_sprites.custom_draw(self.player)
 
-            self.visible_sprites.custom_draw(self.player)
+                # self.entities.draw(self.screen)
 
-            # self.entities.draw(self.screen)
+                # После отрисовки всего, переворачиваем экран
+                pygame.display.flip()
+            pygame.quit()
 
-            # После отрисовки всего, переворачиваем экран
-            pygame.display.flip()
-        pygame.quit()
 
-show_menu()
+if __name__ == '__main__':
+    pygame.init()
+    Manager = GameManager()
+    Manager.run()
--- menu.py
+++ menu.py
@@ -1,62 +1,4 @@
 import pygame
-import random
-from main import GameManager
-
-pygame.init()
-
-WIDTH = 1280
-HEIGHT = 720
-
-# icon = pygame.image.load('castle.png')
-# GameManager.screen.set_icon(icon)
-
-clock = pygame.time.Clock()
-
-
-def show_menu():
-    menu_bckgr = pygame.image.load('menu.jpg')
-    menu_bckgr = pygame.transform.scale(menu_bckgr, (1280, 720))
-
-    start_btn = Button(265, 80)
-    quit_btn = Button(245, 80)
-
-    show = True
-
-    while show:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        GameManager.screen.blit(menu_bckgr, (0, 0))
-        start_btn.draw(270, 200, 'start game', game_cycle, 50)
-        quit_btn.draw(280, 300, 'quit game', quit, 50)
-
-        pygame.GameManager.screen.update()
-        clock.tick(30)
-
-
-def game_cycle():
-    game = True
-
-    while game:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_ESCAPE]:
-            pause()
-
-
-        GameManager.screen.fill((255, 255, 255))
-
-        pygame.GameManager.screen.update()
-        clock.tick(80)
-
-    return game_over()
 
 
 class Button:
@@ -66,7 +8,7 @@ class Button:
         self.inactive_clr = (13, 162, 58)
         self.active_clr = (23, 204, 58)
 
-    def draw(self, x, y, message, action=None, font_size=30):
+    def draw(self, GameManager, x, y, message, action=None, font_size=30):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
@@ -85,16 +27,40 @@ class Button:
         else:
             pygame.draw.rect(GameManager.screen, self.inactive_clr, (x, y, self.width, self.height))
 
-        print_text(message=message, x=x+10, y=y+10, font_size=font_size)
+        print_text(GameManager, message=message, x=x+10, y=y+10, font_size=font_size)
 
 
-def print_text(message, x, y, font_color=(0, 0, 0), font_type='Empirecrown.ttf', font_size=30):
+def show_menu(GameManager):
+    menu_bckgr = pygame.image.load('./img/menu/menu.jpg')
+    menu_bckgr = pygame.transform.scale(menu_bckgr, (1280, 720))
+
+    start_btn = Button(265, 80)
+    quit_btn = Button(245, 80)
+
+    show = True
+
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        GameManager.screen.blit(menu_bckgr, (0, 0))
+        start_btn.draw(GameManager, 270, 200, 'start game', GameManager.start_game, 50)
+        quit_btn.draw(GameManager, 280, 300, 'quit game', quit, 50)
+
+        # pygame.GameManager.screen.update()
+        pygame.display.flip()
+        GameManager.clock.tick(30)
+
+
+def print_text(GameManager, message, x, y, font_color=(0, 0, 0), font_type='./img/menu/Empirecrown.ttf', font_size=30):
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
     GameManager.screen.blit(text, (x, y))
 
 
-def pause():
+def pause(GameManager):
     paused = True
     while paused:
         for event in pygame.event.get():
@@ -102,32 +68,10 @@ def pause():
                 pygame.quit()
                 quit()
 
-        print_text('paused. press ENTER to continue', 160, 300)
+        print_text(GameManager, 'paused. press ENTER to continue', 160, 300)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RETURN]:
             paused = False
 
-        pygame.screen.update()
-
-
-def game_over():
-    stopped = True
-    while stopped:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        print_text('game over. press ENTER to play again or ESC to exit', 40, 300)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:
-            return True
-        if keys[pygame.K_ESCAPE]:
-            return False
-
-        pygame.GameManager.screen.update()
-        clock.tick(15)
-
-
-pygame.quit()
-# quit()
+        # pygame.screen.update()
+        pygame.display.flip()
