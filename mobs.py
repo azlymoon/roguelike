fe9492e6f25b50dd05ca589400246ce62f300445
DS-oops <63066021+DS-oops@users.noreply.github.com>
2022-05-23 14:45:33 +0300

    Add files via upload

--- mobs.py
+++ mobs.py
@@ -2,10 +2,11 @@ import pygame
 from support import import_folder
 from projectile import Flying_eye_projectile, Goblin_projectile, Mushroom_projectile
 from math import sqrt
+import random
 
 
 class Flying_eye(pygame.sprite.Sprite):
-    def __init__(self, pos, player_pos):
+    def __init__(self, pos, player, obstacle_sprites):
         pygame.sprite.Sprite.__init__(self)
         self.animations = {'idle_left': [], 'idle_right': [],
                            'run_left': [], 'run_right': [],
@@ -17,7 +18,7 @@ class Flying_eye(pygame.sprite.Sprite):
         self.frame_index = 0
         self.image = self.animations['idle_right'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
-
+        self.player = player
         # mob movement
 
         self.direction = pygame.math.Vector2(0, 0)
@@ -25,13 +26,15 @@ class Flying_eye(pygame.sprite.Sprite):
         self.health = 1
         self.coordx = pos[0]
         self.coordy = pos[1]
-        self.playerx = player_pos[0]
-        self.playery = player_pos[1]
+
         # mob status
         self.status = 'idle_right'
         self.attack_status = 0
         self.next_status = 0
-        self.projectile = Flying_eye_projectile(pos, player_pos)
+        self.projectile = Flying_eye_projectile(pos=pos, player=player)
+        self.projectile.status = 'explode'
+        self.obstacle_sprites = obstacle_sprites
+        self.move_time = 1
 
     def import_assets(self):
         path = './flying_eye/'
@@ -49,27 +52,20 @@ class Flying_eye(pygame.sprite.Sprite):
             self.image = animation[int(self.frame_index)]
 
     def get_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            self.direction.x = 1
-        elif keys[pygame.K_a]:
-            self.direction.x = -1
-        elif keys[pygame.K_w]:
-            self.direction.y = -1
-        elif keys[pygame.K_s]:
-            self.direction.y = 1
-        elif keys[pygame.K_SPACE]:
-            self.attack_status = 1
+        #print(type(self.player.coordx))
+        #print(type(self.coordx))
+        if self.move_time % 151 == 0:
+            self.direction.x = random.randint(-1, 1)
+            self.direction.y = random.randint(-1, 1)
         else:
             self.direction.x = 0
             self.direction.y = 0
-            self.attack_status = 0
+            self.move_time += 1
 
     def get_status(self):
-        if sqrt((self.coordx - self.playerx) ** 2 + (self.coordy - self.playery) ** 2) < 160:
+        if sqrt((self.coordx - self.player.coordx) ** 2 + (self.coordy - self.player.coordy) ** 2) < 160:
+            self.create_projectile([self.coordx, self.coordy], [self.player.coordx, self.player.coordy])
             self.attack_status = 1
-            # self.projectile = Flying_eye_projectile((self.coordx, self.coordy), player_pos)
-            # self.projectile.status = 'fly'
         if self.attack_status == 1:
             if self.status == 'idle_right':
                 self.status = 'attack_right'
@@ -109,29 +105,39 @@ class Flying_eye(pygame.sprite.Sprite):
     def create_projectile(self, mob_coords, player_coords):
         self.projectile = Flying_eye_projectile(mob_coords, player_coords)
 
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+
     def update(self):
         self.get_input()
         self.get_status()
         self.rect.x += self.direction.x * self.speed
+        self.collision('horizontal')
         self.rect.y += self.direction.y * self.speed
+        self.collision('vertical')
         self.coordx += self.direction.x * self.speed
         self.coordy += self.direction.y * self.speed
         self.animate()
 
 
-# class Skeleton(Flying_eye):
-#     def import_assets(self):
-#         path = './img_mobs/skeleton/'
-#
-#         for animation in self.animations.keys():
-#             full_path = path + animation
-#             self.animations[animation] = import_folder(full_path)
-
-
 class Goblin(Flying_eye):
-    def __init__(self, pos, player_pos):
-        super().__init__(pos=pos, player_pos=player_pos)
-        self.projectile = Goblin_projectile(pos=pos, player_pos=player_pos)
+    def __init__(self, pos, player, obstacle_sprites):
+        super().__init__(pos=pos, player=player, obstacle_sprites=obstacle_sprites)
+        self.projectile = Goblin_projectile(pos=pos, player=self.player)
 
     def import_assets(self):
         path = './goblin/'
@@ -143,10 +149,11 @@ class Goblin(Flying_eye):
     def create_projectile(self, mob_coords, player_coords):
         self.projectile = Goblin_projectile(mob_coords, player_coords)
 
+
 class Mushroom(Flying_eye):
-    def __init__(self, pos, player_pos):
-        super().__init__(pos=pos, player_pos=player_pos)
-        self.projectile = Mushroom_projectile(pos=pos, player_pos=player_pos)
+    def __init__(self, pos, player, obstacle_sprites):
+        super().__init__(pos=pos, player=player, obstacle_sprites=obstacle_sprites)
+        self.projectile = Mushroom_projectile(pos=pos, player=self.player)
 
     def import_assets(self):
         path = './mushroom/'
